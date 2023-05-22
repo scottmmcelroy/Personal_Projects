632df61331e0bb6a0956560754569808addd1671
scottmmcelroy <134321103+scottmmcelroy@users.noreply.github.com>
2023-05-22 17:22:06 -0400

    Update ads1247.h

--- ads1247.h
+++ ads1247.h
@@ -22,6 +22,7 @@
 //*****************/
 // Defines
 //*****************/
+#defin RTC_START 0x20 //start pin is P2.0
 //******RTC COMMANDS******
 //****ADS1247 commands
 #define RTC_WAKEUP 0x01 //exit power-down mode
